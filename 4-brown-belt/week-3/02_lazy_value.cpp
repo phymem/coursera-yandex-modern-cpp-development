e7718e1b02ee049acb4dcb8b2b5530f6d9736551
Alexander Garanin <phymem@gmail.com>
2020-04-22 11:13:54 +0300

    week 3 of brown-belt: lazy_value.cpp

---
+++ 4-brown-belt/week-3/02_lazy_value.cpp
@@ -0,0 +1,54 @@
+#include "test_runner.h"
+
+#include <functional>
+#include <string>
+using namespace std;
+
+template <typename T>
+class LazyValue {
+public:
+	explicit LazyValue(std::function<T()> init) :
+		init_func_(std::move(init)) {}
+
+	bool HasValue() const { return value_.has_value(); }
+	const T& Get() const {
+		if (!value_) {
+			value_ = init_func_();
+		}
+		return *value_;
+	}
+
+private:
+
+	mutable std::optional<T> value_;
+	std::function<T()> init_func_;
+};
+
+void UseExample() {
+	const string big_string = "Giant amounts of memory";
+
+	LazyValue<string> lazy_string([&big_string] { return big_string; });
+
+	ASSERT(!lazy_string.HasValue());
+	ASSERT_EQUAL(lazy_string.Get(), big_string);
+	ASSERT_EQUAL(lazy_string.Get(), big_string);
+}
+
+void TestInitializerIsntCalled() {
+	bool called = false;
+
+	{
+		LazyValue<int> lazy_int([&called] {
+			called = true;
+			return 0;
+		});
+	}
+	ASSERT(!called);
+}
+
+int main() {
+	TestRunner tr;
+	RUN_TEST(tr, UseExample);
+	RUN_TEST(tr, TestInitializerIsntCalled);
+	return 0;
+}
